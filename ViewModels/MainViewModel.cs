52348bbaf6a69a306de4a4f4dd265b8bf8a59802
simkaa47 <simkaa7@gmail.com>
2022-10-18 09:53:26 +0300

    Add project files.

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using DataAccess;
+using DataAccess.Models;
+using System.Collections.Generic;
+
+namespace RTK_HMI.ViewModels
+{
+    internal class MainViewModel : PropertyChangedBase
+    {
+        #region Параметры
+        private IEnumerable<Parameter> _parameters;
+        public IEnumerable<Parameter> Parameters
+        {
+            get => _parameters;
+            set => Set(ref _parameters, value);
+        } 
+        #endregion
+
+        void InitParameters()
+        {
+
+        }
+    }
+}
